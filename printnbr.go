d80554b19c5338cf170d64616f459970f9329187
iyoussef <ibraheemyoussefiy@gmail.com>
2023-01-28 16:33:03 +0000

    Q2

---
+++ printnbr.go
@@ -0,0 +1,27 @@
+package piscine
+
+// import "github.com/01-edu/z01"
+
+/* func PrintNbr(n int) {
+	var save int
+	var tableau []int
+	var conversion int
+	if n < 0 {
+		z01.PrintRune('-')
+	}
+	if n == 0 {
+		z01.PrintRune('0')
+	}
+	for n != 0 {
+		save = n % 10
+		if save < 0 {
+			save = -save
+		}
+		tableau = append(tableau, save)
+		n /= 10
+	}
+	for i := len(tableau) - 1; i >= 0; i-- {
+		conversion = tableau[i] + 48
+		z01.PrintRune(rune(conversion))
+	}
+} */
